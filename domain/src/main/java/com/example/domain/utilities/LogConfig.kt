21f5a8d18f9dcffbd97fccecdbceb99c48a2688d
kaniffoll <stakot2902@gmail.com>
2025-07-01 13:25:52 +0300

    commit 22

--- domain/src/main/java/com/example/domain/utilities/LogConfig.kt
+++ domain/src/main/java/com/example/domain/utilities/LogConfig.kt
@@ -8,12 +8,14 @@ object LogConfig {
     const val APP_TAG = "TASK_1_COMPOSE_APP"
 }
 
+@Suppress("unused")
 fun Any.e(appTag: String = APP_TAG) {
     if (BuildConfig.DEBUG) {
         Log.e(appTag, this.toString())
     }
 }
 
+@Suppress("unused")
 fun Any.d(appTag: String = APP_TAG) {
     if (BuildConfig.DEBUG) {
         Log.d(appTag, this.toString())
